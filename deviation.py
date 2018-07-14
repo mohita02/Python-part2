6923560c5adc97ab2052954cebf7173a59d99816
Mohita Chaudhary <mohitachaudhary02@gmail.com>
2018-07-14 22:30:32 +0530

    Add files via upload

---
+++ deviation.py
@@ -0,0 +1,7 @@
+import math
+a = int(input("enter first number"))
+b = int(input("enter the second number"))
+deviation_a = (((a+b)/2)-a)
+print(deviation_a,"Deviation of a")
+deviation_b = (((a+b)/2)-b)
+print(deviation_b,"Deviation of b")
