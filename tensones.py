f6df9ce9915af0bbcf064becb215d5c6a6887411
Mohita Chaudhary <mohitachaudhary02@gmail.com>
2018-07-14 22:31:40 +0530

    Add files via upload

---
+++ tensones.py
@@ -0,0 +1,8 @@
+import math
+a = int(input("enter any number to find their ones place digit"))
+c = int(input("enter any number to find their tens place digit"))
+ones_a = a%10
+print(ones_a,"ones place digit")
+b = c//10
+tens_c = b%10
+print(tens_c,"tens place digit")
