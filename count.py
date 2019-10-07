c30bfafe227e4ab1c1d0f9dc4105805cd611bf33
Govind85 <krishnagovind8@gmail.com>
2019-10-07 16:12:47 +0530

    Update count.py

--- count.py
+++ count.py
@@ -1,8 +1,8 @@
 def list_count(nums):
 	count = 0
 	for num in nums:
-		if num==4:
+		if num==1:
 			count=count+1
 	return count
-print(list_count([1,4,6,7,3,4,9]))
+print(list_count([1,4,6,7,3,1,1]))
 
